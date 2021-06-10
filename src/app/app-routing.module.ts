f7b3e2e3f532e2b582e4fe9828cc73f85c0e6acc
Purple Squirrel <33865731+mmbm2leej@users.noreply.github.com>
2021-06-10 13:29:55 -0400

    mainpage changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddContactComponent } from './add-contact/add-contact.component';
 import { LoginComponent } from './login/login.component';
+import { MainSectionComponent } from './main-section/main-section.component';
 import { MainpageComponent } from './mainpage/mainpage.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   { path: 'mainpage', component: MainpageComponent },
-  {path: '', redirectTo: 'login', pathMatch: 'full'}
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  { path: 'add-contact', component: AddContactComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MainpageComponent } from './mainpage/mainpage.component';
+import { NotesComponent } from './notes/notes.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ProviderComponent } from './provider/provider.component';
+import { MainSectionComponent } from './main-section/main-section.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    MainpageComponent
+    MainpageComponent,
+    NotesComponent,
+    ContactsComponent,
+    ProviderComponent,
+    MainSectionComponent,
+    AddContactComponent
   ],
   imports: [
     BrowserModule,
