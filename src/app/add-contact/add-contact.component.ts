d53f08b6433f62d060c79c39a6630b6edae532c8
Purple Squirrel <33865731+mmbm2leej@users.noreply.github.com>
2021-06-14 10:36:45 -0400

    addcontact works, tweaked addnote functionality

--- src/app/add-contact/add-contact.component.ts
+++ src/app/add-contact/add-contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 @Component({
@@ -7,11 +8,23 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./add-contact.component.css']
 })
 export class AddContactComponent implements OnInit {
-    contactModel: any;
+    contactModel: any = {
+        "title": "",
+        "first_name": "",
+        "last_name": "",
+        "mobile_phone": "",
+        "office_phone": "",
+        "office_ext": "",
+        "fax": "",
+        "toll_free": "",
+        "toll_free_ext": "",
+        "email": ""
+    }
     currentUser: any;
     errorMessage: any;
+    submitContactModel: any;
 
-  constructor(private authServ: AuthService) { }
+  constructor(private authServ: AuthService, private router: Router) { }
 
   ngOnInit(): void {
     this.authServ.getCurrentUser().subscribe(
@@ -20,6 +33,49 @@ export class AddContactComponent implements OnInit {
         },
         (error) => this.errorMessage = error
     )      
+  }
+  
+    changeTitle(title:string) {
+        this.contactModel.title = title;
+    }
+
+  submitNewContact(contactForm: any) {
+    this.submitContactModel = {
+        "title": this.contactModel.title,
+        "first_name": this.contactModel.first_name,
+        "last_name": this.contactModel.last_name,
+        "mobile_phone": this.contactModel.mobile_phone,
+        "office_phone": this.contactModel.office_phone,
+        "office_ext": this.contactModel.office_ext,
+        "fax": this.contactModel.fax,
+        "toll_free": this.contactModel.toll_free,
+        "toll_free_ext": this.contactModel.toll_free_ext,
+        "email": this.contactModel.email
+    }
+
+    console.log(this.submitContactModel);
+
+    this.currentUser.contacts.push(this.submitContactModel);
+
+
+    this.authServ.updateUser(this.currentUser).subscribe(
+        (data) => {
+            this.currentUser = data;
+            },
+            (error) => this.errorMessage = error
+    )
+
+    // this.authServ.updateUser(updatedUser).subscribe(
+    //     (data) => {
+    //         this.currentUser = data;
+    //     },
+    //     (error) => this.errorMessage = error;
+    // )
+
+    
+
+
+    this.router.navigate( ['main-section']);
   }
 
 }
