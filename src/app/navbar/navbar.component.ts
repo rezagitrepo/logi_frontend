956aade53e8a9c02774dc6607edaf3854591651c
rezagitrepo <reza.ali.cse@gmail.com>
2021-06-15 16:36:29 -0500

    commit at 4pm

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -9,9 +9,25 @@ import { AuthService } from '../services/auth.service';
 })
 export class NavbarComponent implements OnInit {
 
+  currentUser: any;
+  private errorMessage: any;
+  // prov_is_active: boolean = false;
+  // prov_rate_signed: boolean = false;
+  // prov_primary: boolean = false;
+
   constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+
+    this.authService.getCurrentUser().subscribe(
+      (data) => {
+          this.currentUser = data;
+          // this.prov_is_active = this.currentUser.provider[0].active;
+          // this.prov_rate_signed = this.currentUser.provider[0].rate_signed;
+          // this.prov_primary = this.currentUser.provider[0].primary;
+      },
+      (error) => this.errorMessage = error
+  )
   }
 
   logout() {
